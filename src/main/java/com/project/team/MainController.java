e504fcc97b207857fa9d721fbc3483bd2b0ae445
njk7740 <njk7740@gmail.com>
2023-11-14 17:08:57 +0900

    map

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -1,7 +1,6 @@
 package com.project.team;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,13 +8,6 @@ public class MainController {
     @GetMapping("/")
     public String index() {
         return "start";
-    }
-
-    @GetMapping("/map")
-    public String map(Model model, String inputAddress, String myLocation) {
-        model.addAttribute("inputAddress", inputAddress);
-        model.addAttribute("myLocation", myLocation);
-        return "map";
     }
 
     @GetMapping("/main")
