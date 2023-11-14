103936b066f04b7b14894b41e8717abdf20d7493
dbstjwjd <wjddbstj1@naver.com>
2023-11-14 16:05:28 +0900

    ass

--- src/main/java/com/project/team/Restaurant/RestaurantController.java
+++ src/main/java/com/project/team/Restaurant/RestaurantController.java
@@ -23,12 +23,12 @@ public class RestaurantController {
 
     private final SiteUserService siteUserService;
 
-    @PreAuthorize("isAuthenticated()")
     @GetMapping("/register")
     public String register() {
         return "registerForm";
     }
 
+    /*
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/register")
     public String register(Principal principal, @Valid RestaurantRegisterForm restaurantRegisterForm, BindingResult bindingResult) {
@@ -39,5 +39,6 @@ public class RestaurantController {
         this.restaurantService.registerRestaurant(restaurantRegisterForm.getName(), restaurantRegisterForm.getAddress(),
                 restaurantRegisterForm.getNumber(),user);
         return "redirect:/map";
-    }
+        }
+     */
 }
