e84da50cc8e19d3a35ffbb098656217b0a06cc26
dbstjwjd <wjddbstj1@naver.com>
2023-11-16 08:44:43 +0900

    sdfs

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
     public String main() {
         return "main";
     }
+
+    @GetMapping("/regTest")
+    public String regTest() {
+        return "registerForm";
+    }
 }
