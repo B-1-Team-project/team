d074f9fe5328ceee6fcfbe4bfb718bb871057bcc
njk7740 <njk7740@gmail.com>
2023-11-16 08:54:59 +0900

    map

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class MainController {
     @GetMapping("/")
@@ -22,8 +19,7 @@ public class MainController {
     }
 
     @GetMapping("/test")
-    public String testPage(Model model) {
-        List<String> x = new ArrayList<>();
+    public String testPage() {
         return "test";
     }
 }
