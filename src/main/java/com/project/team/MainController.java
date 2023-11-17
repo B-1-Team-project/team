5a8b5a0a56b9ef1c7272e8d006e5373b0688ade2
njk7740 <njk7740@gmail.com>
2023-11-17 17:38:17 +0900

    getDatas

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -1,11 +1,19 @@
 package com.project.team;
 
+import com.project.team.Restaurant.Restaurant;
 import com.project.team.Restaurant.RestaurantService;
+import com.project.team.User.SiteUser;
 import com.project.team.User.SiteUserService;
 import com.project.team.User.UserCreateForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,5 +39,22 @@ public class MainController {
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @RequestMapping(value = "/test/receive", method = RequestMethod.POST)
+    public String receive(@RequestBody List<Map<String, String>> result) {
+        SiteUser user = siteUserService.getUser("njk7740");
+        for (Map<String, String> data : result) {
+            Restaurant restaurant = restaurantService.registerRestaurant(
+                    data.get("place_name"),
+                    data.get("road_address_name"),
+                    data.get("phone"),
+                    null,
+                    null,
+                    user
+            );
+            restaurantService.setLocation(restaurant, data.get("y"), data.get("x"));
+        }
+        return "/main";
     }
 }
