7e13a3f50c1b61c13d5552d4c4f996ca783bb5f4
zzanggajacko <zzanggajacko@gmail.com>
2023-11-20 16:52:04 +0900

    최신화

--- src/main/java/com/project/team/User/OAuthAttributes.java
+++ src/main/java/com/project/team/User/OAuthAttributes.java
@@ -56,12 +56,6 @@ public class OAuthAttributes {
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");    // 네이버에서 받은 데이터에서 프로필 정보다 담긴 response 값을 꺼낸다.
 
-        System.out.println("naver " + attributes);
-        System.out.println("id " + response.get("id"));
-        System.out.println("userNameAttributeName " + userNameAttributeName);
-        System.out.println("attributes.get(userNameAttributeName) " + attributes.get(userNameAttributeName));
-
-
         return new OAuthAttributes((String) response.get("id"),
                 (Map<String, Object>) attributes.get(userNameAttributeName),
                 "id",
@@ -81,8 +75,6 @@ public class OAuthAttributes {
                 (String) attributes.get("picture"));
     }
 
-    // .. getter/setter 생략
-
     public SiteUser toEntity() {
         String id = null;
         if (nameAttributeKey.equals("response")) {
@@ -90,8 +82,4 @@ public class OAuthAttributes {
         } else id = attributes.get(nameAttributeKey).toString();
         return new SiteUser(id, name, email, picture, LocalDateTime.now());
     }
-//    public SiteUser toEntity(){
-//        return new SiteUser(id, name, email, picture, LocalDateTime.now());
-//    }
-
 }
