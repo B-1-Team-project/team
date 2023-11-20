728f5915a81b902b3a13f00f71b1956bb916bb89
dbstjwjd <wjddbstj1@naver.com>
2023-11-20 17:48:30 +0900

    wer

--- src/test/java/com/project/team/TeamApplicationTests.java
+++ src/test/java/com/project/team/TeamApplicationTests.java
@@ -1,13 +1,24 @@
 package com.project.team;
 
+import com.project.team.Reservation.Reservation;
+import com.project.team.Reservation.ReservationService;
+import com.project.team.Restaurant.Restaurant;
+import com.project.team.Restaurant.RestaurantService;
+import com.project.team.User.SiteUser;
+import com.project.team.User.SiteUserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @SpringBootTest
 class TeamApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
