446abe54ea05d6d28576a418fbe74f5a4142f551
zzanggajacko <zzanggajacko@gmail.com>
2023-11-22 12:49:07 +0900

    1

--- src/main/java/com/project/team/User/SiteUserController.java
+++ src/main/java/com/project/team/User/SiteUserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
