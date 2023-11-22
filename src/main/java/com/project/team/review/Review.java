99b8880f4b721e618c3f07604e66125cc9f1e921
dbstjwjd <wjddbstj1@naver.com>
2023-11-22 12:38:28 +0900

    Sdf

--- src/main/java/com/project/team/review/Review.java
+++ src/main/java/com/project/team/review/Review.java
@@ -15,11 +15,16 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @ManyToOne
     private Restaurant restaurant;
+
     @ManyToOne
     private SiteUser user;
+
     private String comment;
+
     private Integer star;
+
     private LocalDateTime regDate;
 }
