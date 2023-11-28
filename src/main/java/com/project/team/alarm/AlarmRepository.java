2f757bedc0aa6689c3deffbaa48f8ebc1554a945
njk7740 <143564945+njk7740@users.noreply.github.com>
2023-11-28 12:46:09 +0900

    Chat (#73)
    * chat
    
    * chat
    
    * chat
    
    * chat
    
    * chat
    
    ---------
    
    Co-authored-by: njk7740 <njk7705@gmail.com>

--- src/main/java/com/project/team/alarm/AlarmRepository.java
+++ src/main/java/com/project/team/alarm/AlarmRepository.java
@@ -2,9 +2,10 @@ package com.project.team.alarm;
 
 import com.project.team.User.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
-    List<Alarm> findByUser(SiteUser user);
+    List<Alarm> findByUserOrderByRegDateDesc(SiteUser user);
+    List<Alarm> findByUserAndTargetAndTypeAndConfirm(SiteUser user, SiteUser target, String type, Boolean confirm);
+    List<Alarm> findByUserAndChatRoom(SiteUser user, String room);
 }
--- src/main/java/com/project/team/chat/ChatController.java
+++ src/main/java/com/project/team/chat/ChatController.java
@@ -1,42 +1,58 @@
-package com.project.team.socket;
+package com.project.team.chat;
 
 import com.project.team.Restaurant.Restaurant;
 import com.project.team.Restaurant.RestaurantService;
 import com.project.team.User.SiteUser;
 import com.project.team.User.SiteUserService;
+import com.project.team.alarm.Alarm;
+import com.project.team.alarm.AlarmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/chat")
 public class ChatController {
     private final SiteUserService userService;
     private final RestaurantService restaurantService;
+    private final ChatService chatService;
+    private final AlarmService alarmService;
 
-    @GetMapping("/chat/toOwner/{id}")
+    @GetMapping("/create/{id}")
     @PreAuthorize("isAuthenticated()")
     public String chatToOwner(@PathVariable(value = "id") Integer id, Model model, Principal principal) {
         Restaurant restaurant = restaurantService.getRestaurant(id);
         SiteUser user = userService.getUser(principal.getName());
         SiteUser owner = restaurant.getOwner();
+        String room = UUID.randomUUID().toString();
         model.addAttribute("user", user);
-        model.addAttribute("owner", owner);
+        model.addAttribute("target", owner);
+        model.addAttribute("room", room);
         return "chat";
     }
 
-    @GetMapping("/chat/toCustomer/{id}")
+    @GetMapping("/join/{room}")
     @PreAuthorize("isAuthenticated()")
-    public String chatToCustomer(@PathVariable(value = "id") String id, Model model, Principal principal) {
-        SiteUser user = userService.getUser(id);
-        SiteUser owner = userService.getUser(principal.getName());
+    public String chatToCustomer(@PathVariable(value = "room") String room, Model model, Principal principal) {
+        SiteUser user = userService.getUser(principal.getName());
+        SiteUser target = chatService.getByTarget(room, user);
+        List<Alarm> alarmList = alarmService.getByRoom(user, room);
+
+        for (Alarm alarm : alarmList) alarmService.confirm(alarm);
+
         model.addAttribute("user", user);
-        model.addAttribute("owner", owner);
+        model.addAttribute("target", target);
+        model.addAttribute("room", room);
+        model.addAttribute("chatList", chatService.getByRoom(room));
         return "chat";
     }
 }
