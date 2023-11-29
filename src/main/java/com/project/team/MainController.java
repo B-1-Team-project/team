c8b0970b85aea9d3ba65998fe485c9c1db1b5950
dbstjwjd <143564887+dbstjwjd@users.noreply.github.com>
2023-11-29 17:32:42 +0900

    Dbstjwjd (#79)
    * dbstjwjd
    
    * wak
    
    * 평균별점
    
    * 리뷰사진등록
    
    * 오류수정
    
    * 리뷰댓글진행중
    
    * ㅠㅠ
    
    * 리뷰 댓글 완료
    
    * ì중간수정
    
    * ui수정
    
    * ui또수정
    
    * a
    
    * sssss

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -1,5 +1,7 @@
 package com.project.team;
 
+import com.project.team.User.SiteUser;
+import com.project.team.User.SiteUserService;
 import com.project.team.User.UserCreateForm;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
@@ -10,12 +12,19 @@ import org.jsoup.Jsoup;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
+
+    private final SiteUserService siteUserService;
 
     @GetMapping("/")
     public String index(UserCreateForm userCreateForm) {
@@ -33,7 +42,7 @@ public class MainController {
         JSONParser jsonParser = new JSONParser();
         JSONObject detail = (JSONObject) jsonParser.parse(doc);
         JSONObject basicInfo = (JSONObject) detail.get("basicInfo");
-        String target = ((JSONObject)((JSONArray)((JSONObject)((JSONArray)((JSONObject)basicInfo.get("openHour")).get("periodList")).get(0)).get("timeList")).get(0)).get("timeSE").toString();
+        String target = ((JSONObject) ((JSONArray) ((JSONObject) ((JSONArray) ((JSONObject) basicInfo.get("openHour")).get("periodList")).get(0)).get("timeList")).get(0)).get("timeSE").toString();
         model.addAttribute("data", detail);
         return "test";
     }
@@ -42,4 +51,20 @@ public class MainController {
     public String interProcess() {
         return "interprocess";
     }
+
+
+    @GetMapping("/weatherMenu")
+    public String weatherMenu(Model model, Principal principal) {
+        if (principal != null) {
+            SiteUser user = this.siteUserService.getUser(principal.getName());
+            model.addAttribute("user", user);
+        }
+        return "weatherMenu";
+    }
+
+    @PostMapping("/recommendMenu")
+    public void recommendMenu(@RequestBody List<Map<String, String>> dataList) {
+
+    }
+
 }
---
+++ src/main/resources/static/weather.js
@@ -0,0 +1,259 @@
+const apiKey = 'pS2UrTSQaUinqkDWsuCq9%2BWXge1G%2FBtY5Buv8BSU0NUu8lrVoa18K3XiyP9Aw3v3jEbJ%2BmbaFqGysTiZelVklA%3D%3D';
+
+var RE = 6371.00877; // 지구 반경(km)
+var GRID = 5.0; // 격자 간격(km)
+var SLAT1 = 30.0; // 투영 위도1(degree)
+var SLAT2 = 60.0; // 투영 위도2(degree)
+var OLON = 126.0; // 기준점 경도(degree)
+var OLAT = 38.0; // 기준점 위도(degree)
+var XO = 43; // 기준점 X좌표(GRID)
+var YO = 136; // 기준점 Y좌표(GRID)
+
+function success(position) {
+   const lat = position.coords.latitude;
+   const lon = position.coords.longitude;
+   const currentDateTime = getCurrentDateTime();
+
+   var grid = dfs_xy_conv("toXY", lat, lon);
+   var url = new URL("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst");
+   getWeather(apiKey, grid.x, grid.y);
+   var params = {
+     serviceKey: apiKey,
+     pageNo: 1,
+     numOfRows: 60,
+     dataType: 'json',
+     base_date: currentDateTime.date,
+     base_time: currentDateTime.time,
+     nx: grid.x,
+     ny: grid.y
+   };
+
+   url.search = new URLSearchParams(params).toString();
+
+   fetch(decodeURI(url))
+     .then(res => {
+     return res.json();
+   })
+     .then(data => {
+     const dataList = data.response.body.items.item;
+
+     $.ajax({
+        url: '/recommendMenu',
+        type: 'post',
+        data: dataList
+     });
+
+     handleSkyData(dataList[18].fcstValue, dataList[6].fcstValue);
+     handleTemperatureData(dataList[24].fcstValue);
+     handlePrecipitationData(dataList[6].fcstValue);
+
+     function handleSkyData(skyValue, precipitationValue) {
+         const skyElement = document.querySelector("#sky");
+         const skyImage = document.getElementById("skyStatus");
+
+         switch (skyValue) {
+             case 1:
+                 skyElement.innerText = "맑고";
+                    if (![1, 2, 3].includes(precipitationValue)) {
+                        skyImage.src = "/image/weather/쨍쨍.png";
+                    }
+                 break;
+             case 2:
+                 skyElement.innerText = "구름이 많고";
+                 if (![1, 2, 3].includes(precipitationValue)) {
+                     skyImage.src = "/image/weather/구름많음.png";
+                 }
+                 break;
+             default:
+                 skyElement.innerText = "흐리고";
+                 if (![1, 2, 3].includes(precipitationValue)) {
+                     skyImage.src = "/image/weather/흐림.png";
+                 }
+                 break;
+         }
+     }
+
+     function handleTemperatureData(tempValue) {
+         const tempElement1 = document.querySelector("#temp1");
+         const tempElement2 = document.querySelector("#temp2");
+         const tempImage = document.getElementById("tempStatus");
+         tempElement1.innerText = `${tempValue}℃`;
+         if (tempValue < 0) {
+             tempElement2.innerText = `매우 추운 날씨`;
+             tempImage.src = "/image/weather/추워온도계.png";
+         } else if (tempValue >= 0 && tempValue < 10) {
+             tempElement2.innerText = `추운 날씨`;
+             tempImage.src = "/image/weather/추운사람.png";
+         } else if (tempValue >= 10 && tempValue < 20) {
+             tempElement2.innerText = `선선한 날씨`;
+             tempImage.src = "/image/weather/딱좋아온도계.png";
+         } else if (tempValue >= 20 && tempValue < 25) {
+             tempElement2.innerText = `따뜻한 날씨`;
+             tempImage.src = "/image/weather/딱좋아온도계.png";
+         } else if (tempValue >= 25 && tempValue < 30) {
+             tempElement2.innerText = `더운 날씨`;
+             tempImage.src = "/image/weather/더운사람.png";
+         } else {
+             tempElement2.innerText = `매우 더운 날씨`;
+             tempImage.src = "/image/weather/더워온도계";
+         }
+     }
+
+     function handlePrecipitationData(rainValue) {
+         const rainElement = document.querySelector("#rain");
+         const skyImage = document.getElementById("skyStatus");
+
+         switch (rainValue) {
+             case 1:
+                 rainElement.innerText = "현재 비가 오고 있습니다.";
+                 skyImage.src = "/image/weather/비.png";
+                 break;
+             case 2:
+                 rainElement.innerText = "현재 비 또는 눈이 오고 있습니다.";
+                 skyImage.src = "/image/weather/눈이나 비.png";
+                 break;
+             case 3:
+                 rainElement.innerText = "현재 눈이 오고 있습니다.";
+                 skyImage.src = "/image/weather/눈.png";
+                 break;
+         }
+     }
+
+   })
+     .catch(err => {
+     console.log('Fetch Error', err);
+   });
+
+}
+
+function add30Minutes(timeString) {
+    const hours = parseInt(timeString.substring(0, 2), 10);
+    const minutes = parseInt(timeString.substring(2), 10);
+
+    const newHours = hours + Math.floor((minutes + 30) / 60);
+    const newMinutes = (minutes + 30) % 60;
+
+    const newTimeString = `${newHours.toString().padStart(2, '0')}${newMinutes.toString().padStart(2, '0')}`;
+
+    return newTimeString;
+}
+
+function error() {
+   alert("위치 정보를 불러올 수 없습니다.");
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+function getCurrentDateTime() {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    let hours = now.getHours();
+    let minutes = '30';
+
+    if (now.getMinutes() < 30) {
+        hours -= 1;
+    }
+
+    if (hours < 0) {
+        hours = '00';
+    } else {
+        hours = hours.toString().padStart(2, '0');
+    }
+
+    return {
+        date: `${year}${month}${day}`,
+        time: `${hours}${minutes}`
+    };
+}
+
+function roundDownToHalfHour(date) {
+    const roundedDate = new Date(date);
+    roundedDate.setMinutes(roundedDate.getMinutes() - (roundedDate.getMinutes() % 30));
+    return roundedDate;
+}
+
+
+
+function getWeather(apiKey, lat, lon) {
+    const baseUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst";
+
+    const currentDateTime = getCurrentDateTime();
+
+    const url = `${baseUrl}?serviceKey=${apiKey}&pageNo=1&numOfRows=60&dataType=JSON&base_date=${currentDateTime.date}&base_time=${currentDateTime.time}&nx=${lat}&ny=${lon}`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.response);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function dfs_xy_conv(code, v1, v2) {
+    var DEGRAD = Math.PI / 180.0;
+    var RADDEG = 180.0 / Math.PI;
+
+    var re = RE / GRID;
+    var slat1 = SLAT1 * DEGRAD;
+    var slat2 = SLAT2 * DEGRAD;
+    var olon = OLON * DEGRAD;
+    var olat = OLAT * DEGRAD;
+
+    var sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+    var sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
+    var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+    ro = re * sf / Math.pow(ro, sn);
+    var rs = {};
+    if (code == "toXY") {
+        rs['lat'] = v1;
+        rs['lng'] = v2;
+        var ra = Math.tan(Math.PI * 0.25 + (v1) * DEGRAD * 0.5);
+        ra = re * sf / Math.pow(ra, sn);
+        var theta = v2 * DEGRAD - olon;
+        if (theta > Math.PI) theta -= 2.0 * Math.PI;
+        if (theta < -Math.PI) theta += 2.0 * Math.PI;
+        theta *= sn;
+        rs['x'] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+        rs['y'] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+    }
+    else {
+        rs['x'] = v1;
+        rs['y'] = v2;
+        var xn = v1 - XO;
+        var yn = ro - v2 + YO;
+        ra = Math.sqrt(xn * xn + yn * yn);
+        if (sn < 0.0) - ra;
+        var alat = Math.pow((re * sf / ra), (1.0 / sn));
+        alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
+
+        if (Math.abs(xn) <= 0.0) {
+            theta = 0.0;
+        }
+        else {
+            if (Math.abs(yn) <= 0.0) {
+                theta = Math.PI * 0.5;
+                if (xn < 0.0) - theta;
+            }
+            else theta = Math.atan2(xn, yn);
+        }
+        var alon = theta / sn + olon;
+        rs['lat'] = alat * RADDEG;
+        rs['lng'] = alon * RADDEG;
+    }
+    return rs;
+}
+
+
+
