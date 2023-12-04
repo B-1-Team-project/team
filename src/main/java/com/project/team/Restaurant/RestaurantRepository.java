8f06a959ba778ba3780f8b6a072c623b0e799fbc
dbstjwjd <143564887+dbstjwjd@users.noreply.github.com>
2023-12-04 17:51:34 +0900

    Dbstjwjd (#90)
    * dbstjwjd
    
    * wak
    
    * 평균별점
    
    * 리뷰사진등록
    
    * 오류수정
    
    * 리뷰댓글진행중
    
    * ㅠㅠ
    
    * 리뷰 댓글 완료
    
    * ì중간수정
    
    * ui수정
    
    * ui또수정
    
    * a
    
    * sssss
    
    * 고도화
    
    * 추천완
    
    * sss
    
    * 수정
    
    * 중간

--- src/main/java/com/project/team/Restaurant/RestaurantRepository.java
+++ src/main/java/com/project/team/Restaurant/RestaurantRepository.java
@@ -13,6 +13,10 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>
 
     List<Restaurant> findTop3ByOrderByAverageStarDesc();
 
+    @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN FETCH r.reviews ORDER BY SIZE(r.reviews) DESC LIMIT 3")
+    List<Restaurant> findTop3ByOrderByReviewsCountDesc();
+
+
     @Query("SELECT r FROM Restaurant r WHERE r.main LIKE :main1 OR r.main LIKE :main2 OR r.main LIKE :main3 ORDER BY r.averageStar DESC LIMIT 3")
     List<Restaurant> byMain(@Param("main1") String main1, @Param("main2") String main2, @Param("main3") String main3);
 
