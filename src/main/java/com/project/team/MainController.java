c8aedcbaf456d0c6bfc244d2aa3713451634ec70
njk7740 <njk7705@gmail.com>
2023-12-05 11:33:27 +0900

    optimization

--- src/main/java/com/project/team/MainController.java
+++ src/main/java/com/project/team/MainController.java
@@ -4,16 +4,8 @@ package com.project.team;
 import com.project.team.User.SiteUserService;
 import com.project.team.User.UserCreateForm;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.jsoup.Jsoup;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.IOException;
 
 
 @Controller
@@ -29,18 +21,7 @@ public class MainController {
 
     @GetMapping("/main")
     public String mainPage() {
-        return "redirect:/interprocess";
-    }
-
-    @GetMapping("/test")
-    public String test(Model model) throws IOException, ParseException {
-        String doc = Jsoup.connect("https://place.map.kakao.com/main/v/10344306").ignoreContentType(true).execute().body();
-        JSONParser jsonParser = new JSONParser();
-        JSONObject detail = (JSONObject) jsonParser.parse(doc);
-        JSONObject basicInfo = (JSONObject) detail.get("basicInfo");
-        String target = ((JSONObject) ((JSONArray) ((JSONObject) ((JSONArray) ((JSONObject) basicInfo.get("openHour")).get("periodList")).get(0)).get("timeList")).get(0)).get("timeSE").toString();
-        model.addAttribute("data", detail);
-        return "test";
+        return "redirect:/process";
     }
 
     @GetMapping("/process")
