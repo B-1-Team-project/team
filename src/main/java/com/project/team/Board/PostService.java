3972eada288ba27734cd72281e3ea1bc8fa71c8d
habinnn <107250227+habinnn@users.noreply.github.com>
2023-12-07 09:41:09 +0900

    Team1 (#101)
    * reserve
    
    * reserve
    
    * 123
    
    * asd
    
    * s
    
    * j
    
    * habin
    
    * habin
    
    * s
    
    * s
    
    * habinnn
    
    * s
    
    * s
    
    * s
    
    * s

--- src/main/java/com/project/team/Board/PostService.java
+++ src/main/java/com/project/team/Board/PostService.java
@@ -4,10 +4,15 @@ import com.project.team.DataNotFoundException;
 import com.project.team.User.SiteUser;
 import com.project.team.User.SiteUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,5 +53,12 @@ public class PostService {
 
     public void delete(Post post){
         this.postRepository.delete(post);
+    }
+
+    public Page<Post> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,10,Sort.by(sorts));
+        return this.postRepository.findAll(pageable);
     }
 }
